b93880b3ea6031f610d255a1cc2ba50228b1a31f
Susan Lunn <skl1958@g.rit.edu>
2017-10-13 15:25:38 -0400

    added scoreboard stuff

--- CTFd/scoreboard.py
+++ CTFd/scoreboard.py
@@ -1,40 +1,19 @@
 from flask import current_app as app, session, render_template, jsonify, Blueprint, redirect, url_for, request
-from CTFd.utils import unix_time, authed, get_config
+from CTFd.utils import unix_time, authed, get_config, ctf_name
 from CTFd.models import db, Users, Solves, Awards, Challenges, Teams, get_standings
+
+import time
 
 scoreboard = Blueprint('scoreboard', __name__)
 
-# def get_standings(admin=False, count=None):
-#     score = db.func.sum(Challenges.value).label('score')
-#     date = db.func.max(Solves.date).label('date')
-#     scores = db.session.query(Solves.userid.label('userid'), score, date).join(Challenges).group_by(Solves.userid)
-#     awards = db.session.query(Awards.userid.label('userid'), db.func.sum(Awards.value).label('score'), db.func.max(Awards.date).label('date')) \
-#         .group_by(Awards.userid)
-#     results = union_all(scores, awards).alias('results')
-#     sumscores = db.session.query(results.columns.userid, db.func.sum(results.columns.score).label('score'), db.func.max(results.columns.date).label('date')) \
-#         .group_by(results.columns.userid).subquery()
-#     if admin:
-#         standings_query = db.session.query(Users.id.label('userid'), Users.name.label('name'), Users.banned, sumscores.columns.score) \
-#             .join(sumscores, Users.id == sumscores.columns.userid) \
-#             .order_by(sumscores.columns.score.desc(), sumscores.columns.date)
-#     else:
-#         standings_query = db.session.query(Users.id.label('userid'), Users.name.label('name'), sumscores.columns.score) \
-#             .join(sumscores, Users.id == sumscores.columns.userid) \
-#             .filter(Users.banned == False) \
-#             .order_by(sumscores.columns.score.desc(), sumscores.columns.date)
-#     if count is None:
-#         standings = standings_query.all()
-#     else:
-#         standings = standings_query.limit(count).all()
-#     print standings
-#     db.session.close()
-#     return standings
-
-
 @scoreboard.route('/scoreboard')
 def scoreboard_view():
+    errors = []
     if get_config('view_scoreboard_if_authed') and not authed():
         return redirect(url_for('auth.login', next=request.path))
+    if(get_config('start') > time.time()):
+        errors.append('{} scoreboard will be posted once the competition starts'.format(ctf_name()))
+        return render_template('scoreboard.html', errors=errors, teams="")
     standings = get_standings()
     return render_template('scoreboard.html', teams=standings)
 
